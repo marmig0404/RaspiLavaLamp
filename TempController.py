ae54252272e38f092bb1a6290bcde96d9b688c9e
Martin Miglio <marmig0404@gmail.com>
2020-10-29 22:29:04 -0400

    *migrated from gpio to gpiozero

--- TempController.py
+++ TempController.py
@@ -1,6 +1,6 @@
 import glob
 import time
-import RPi.GPIO as GPIO
+import gpiozero
 
 
 class TempController:
@@ -8,29 +8,14 @@ class TempController:
     base_dir = '/sys/bus/w1/devices/'
     device_folder = glob.glob(base_dir + '28*')[0]
     device_file = device_folder + '/w1_slave'
-    cycle_time = 1  # cycle time in seconds
-    frequency = 100  # pwm frequency
 
     def __init__(self, target, heater_pin, sensor_pin, p=1, i=1, d=1):
         self.target = target
         self.heater_pin = heater_pin
-        self.init_heater()
-
-    def __exit__(self, exception_type, exception_value, exception_traceback):
-        GPIO.cleanup()
-
-    def __del__(self):
-        self.remove_heater()
-        
+        self.init_heater()       
 
     def init_heater(self):
-        GPIO.setup(self.heater_pin, GPIO.OUT)
-        self.heater = GPIO.PWM(self.heater_pin, self.frequency)
-        self.heater.start(0)
-
-    def remove_heater(self):
-        self.heater.stop()
-        GPIO.cleanup(self.heater_pin)
+        self.heater = gpiozero.PWMOutputDevice(self.heater_pin)
 
     def update(self):
         temp = self.read_temp()
@@ -38,13 +23,14 @@ class TempController:
         print(temp)
         if temp > self.target :
             print('not heating')
-            self.heater.ChangeDutyCycle(0) 
+            self.heater.off()
         if temp < self.target :
             duty_cycle = ((self.target-temp)*3)+50
             if duty_cycle > 100 : duty_cycle = 100 
             print('heating at duty cycle:')
             print(duty_cycle)
-            self.heater.ChangeDutyCycle(duty_cycle)
+            self.heater.value = duty_cycle/100
+            self.heater.on()
 
     def read_temp(self):
         lines = self.read_temp_raw()
